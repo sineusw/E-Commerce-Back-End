d3d63a4d17faab864b485f66520615bd30d117f4
Winsor Sineus <winsorsineus@Winsors-MacBook-Air.local>
2021-05-10 14:07:16 -0400

    added tables in schema.sql file

--- 02-Homework/Develop/server.js
+++ 02-Homework/Develop/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+require('dotenv').config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
